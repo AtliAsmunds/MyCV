441d5826f93b7d6fefc2d8e9ad213236303d76d0
AtliAsmunds <atliasmunds@gmail.com>
2022-12-19 15:27:09 +0000

    Added new domain to settings

--- my_webpage_project/settings.py
+++ my_webpage_project/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.getenv('DEBUG') == 'TRUE' else False
 
-ALLOWED_HOSTS = ['atliasmunds.pythonanywhere.com', '127.0.0.1']
+ALLOWED_HOSTS = ['atliasmunds.pythonanywhere.com', '127.0.0.1', 'www.atliasmunds.org']
 
 
 # Application definition
